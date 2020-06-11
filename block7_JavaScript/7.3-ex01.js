2f56261e518c7e4b22c15849f2129770edc82d65
Ferrari <luisdiasferrari@gmail.com>
2020-06-11 11:48:08 -0300

    7.3 ex01

--- block7_JavaScript/7.3-ex01.js
+++ block7_JavaScript/7.3-ex01.js
@@ -2,16 +2,22 @@ const assert = require('assert');
 
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+   throw new Error('parameters must be numbers');
+  
   }
 
-  console.log (a + b);
   return a + b;
   
 }
-sum(4, 5);
 
 // implemente seus testes aqui
 assert.strictEqual(sum(4, 5), 9, 'Erro');
 assert.strictEqual(sum(0, 0), 0, 'Erro');
-assert.strictEqual(sum(4, "5"), 'parameters must be numbers', "Error");
+assert.throws(() => {
+  sum(4, '5');
+  });
+assert.throws(() => {
+  sum(4, '5');
+  }, `Error: parameters must be numbers`); 
+
+
