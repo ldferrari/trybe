eb6ee49eec24429ef0476ec763654a58a48fbb66
ferrari <luisdiasferrari@gmail.com>
2020-07-15 19:24:43 -0300

    block12

---
+++ block12_React_estado/12.2-exall/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Formulario from './Formulario';
+
+function App() {
+  return (
+    <div className="App App-header">
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
