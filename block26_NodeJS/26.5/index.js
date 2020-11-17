6c74f4b669f21be1a7a3810d86a579294f252f45
ferrari <luisdiasferrari@gmail.com>
2020-11-17 18:28:34 -0300

    26.5

---
+++ block26_NodeJS/26.5/index.js
@@ -0,0 +1,28 @@
+// Atividade 3:
+// Rotas: /posts/:id e /posts
+// Objetivo: Deve receber uma requisição com o param id e verificar a existência do post com aquele id. Caso exista, retorne os dados daquele post. Caso não exista, 
+// retorne um status de erro com a mensagem id not found.. A rota /posts deve trazer todos os posts cadastrados.
+
+const express = require('express');
+const post = require('./post');
+const app = express();
+
+app.use(express.json());
+
+app.get('/posts', function(req, res, next) {
+  res.status(200).json(post);
+  next();
+});
+
+app.get('/posts/:id', function(req,res, next) {
+  let { id } = req.params;
+  const contem = post.some(e => e.id === Number(id));
+  if (contem) {
+  const qualquer = post.find(e => e.id === Number(id));
+  res.status(200).json(qualquer);
+  } else {
+  res.status(500).send(`id not found..`);
+  }
+});
+
+app.listen(3000);
